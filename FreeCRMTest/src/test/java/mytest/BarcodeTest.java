f9f9a011275b9f560f25f78c1364b491195a08f5
Alley Yu <alleyyu@LEIs-MacBook-Pro.local>
2020-06-24 23:22:03 -0700

    Add BarCodeTest.java

---
+++ FreeCRMTest/src/test/java/mytest/BarcodeTest.java
@@ -0,0 +1,59 @@
+package mytest;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+
+public class BarcodeTest {
+
+	@Test
+	public void barCodeTest() throws IOException, NotFoundException, InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.home")+"/bin/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://barcode.tec-it.com/en");
+		
+//		String inputForBarCode = "AlleyYu";
+		
+//		WebElement dataInput = driver.findElement(By.name("Data"));
+//		dataInput.clear();
+//		dataInput.sendKeys(inputForBarCode);
+//		driver.findElement(By.linkText("Refresh")).click();
+		
+		
+		String barCodeURL = driver.findElement(By.tagName("img")).getAttribute("src");
+		System.out.println(barCodeURL);
+		
+		URL url = new URL(barCodeURL);
+		BufferedImage bufferedImage = ImageIO.read(url);
+		
+		LuminanceSource luminanceSource = new BufferedImageLuminanceSource(bufferedImage);
+		BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(luminanceSource));
+		
+		Result result = new MultiFormatReader().decode(binaryBitmap);
+		System.out.println(result.getText());
+		
+		Assert.assertEquals(result.getText(), "ABC-abc-1234");
+		
+	}
+
+}
