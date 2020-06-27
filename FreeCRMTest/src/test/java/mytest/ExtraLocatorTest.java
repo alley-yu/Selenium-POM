2566ab07f5a54172dfde9991737feaec3f165bca
Alley Yu <alleyyu@LEIs-MacBook-Pro.local>
2020-06-26 17:20:43 -0700

    Add an extra lacator - ByChained

--- FreeCRMTest/src/test/java/mytest/ExtraLocatorTest.java
+++ FreeCRMTest/src/test/java/mytest/ExtraLocatorTest.java
@@ -15,12 +15,13 @@ public class ExtraLocatorTest {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		
-		//by using ByAll, find webelement from left to right by each By until finding it
+		//by using ByAll, find WebElement from left to right by each By until finding it
 		driver.findElement(new ByAll(By.name("firstname"), By.id("u_0_m"), 
 				By.xpath("//input[@name = 'firstname']"))).sendKeys("Alley");
 		
 		driver.findElement(new ByIdOrName("u_0_o")).sendKeys("Yu");
 		
+		//Using ByChained is to allow using parent-child-chaining relation to find WebElement.
 		driver.findElement(new ByChained(By.id("u_0_q"), 
 				By.xpath("//div[@class = 'uiStickyPlaceholderInput uiStickyPlaceholderEmptyInput']"), 
 				By.name("reg_email__"))).sendKeys("alley.yu@hotmail.com");
