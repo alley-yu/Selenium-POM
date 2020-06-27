7544b623cf188aa1387d9fa45a131ab0859e8b52
Alley Yu <alleyyu@LEIs-MacBook-Pro.local>
2020-06-26 23:45:54 -0700

    Add WebDriverManagerTest to use API to setup WebDriver without .exe

---
+++ FreeCRMTest/src/test/java/mytest/WebDriverManagerTest.java
@@ -0,0 +1,38 @@
+package mytest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebDriverManagerTest {
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setup() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		
+	}
+
+	@Test
+	public void checkTitle() {
+		
+		driver.get("https://www.facebook.com/");
+		String title = driver.getTitle();
+		Assert.assertEquals(title, "Facebook - Log In or Sign Up");
+		
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+}
