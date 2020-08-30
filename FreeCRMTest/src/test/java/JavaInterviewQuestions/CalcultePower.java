856181f3e7503e169d1b971802e0eae44d21f5b2
Alley Yu <alleyyu@LEIs-MacBook-Pro.local>
2020-08-29 23:46:06 -0700

    Java Interview: CalculatePower

--- FreeCRMTest/src/test/java/JavaInterviewQuestions/CalcultePower.java
+++ FreeCRMTest/src/test/java/JavaInterviewQuestions/CalcultePower.java
@@ -2,7 +2,7 @@ package JavaInterviewQuestions;
 
 public class CalcultePower {
 	
-	public static long PowerCalculation(int baseNum, int powerNum) {
+	public static long powerCalculation(int baseNum, int powerNum) {
 	
 		long result = 1;
 		for (int i = 1; i <= powerNum; i++) {
@@ -11,14 +11,30 @@ public class CalcultePower {
 		
 		return result;
 	}
+	
+	public static long powerCalculation1(int baseNum, int powerNum) {
+		long result;
+		
+		if (powerNum%2 != 0) {
+			result = baseNum;
+		}else {
+			result =1;
+		}
+		
+		for (int i = 1; i <= powerNum/2; i++) {
+			result = result * baseNum * baseNum;
+		}
+		
+		return result;
+	}
 
 	public static void main(String[] args) {
 		
 		int base = 2;
-		int power = 5;
+		int power = 11;
 		
-		System.out.println(base + "^" + power + " = " + PowerCalculation(base, power));
-		
+		System.out.println(base + "^" + power + " = " + powerCalculation(base, power));
+		System.out.println(base + "^" + power + " = " + powerCalculation1(base, power));
 
 	}
 
